743733bcbe26fc7657e1319abbf37bc2a865f92d
tramallo <timo.22394@hotmail.com>
2023-12-24 16:03:49 -0300

    working react app + socket.io client

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import socketClient from 'socket.io-client'
+
+const socket = socketClient('http://localhost:3000', { transports: ['websocket'] })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
